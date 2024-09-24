e59efbcc4ad7434f2c65a01e33f12637dfccbbd3
Sanogo9160 <mahrokiatou9160@gmail.com>
2024-09-24 21:17:21 +0000

    Structure du projet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import path from "node:path";
+import {DashboardComponent} from "./components/dashboard/dashboard.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  { path: '', component: DashboardComponent },
+];
